3080bc335c643d2b0f487b1ddaf8d3f8e5179f94
Jeffrey Lin <jeffrey@icurse.nl>
2017-11-24 11:12:13 -0500

    aash.c: fork and execute command

--- aash.h
+++ aash.h
@@ -4,9 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+extern pid_t cpid;
+extern int last_wstatus;
+
 
 char *get_input(void);
 char **tokenizer_whitespace(char *string);
+
+void execute_command(char **to_run);
 
 void print_prompt(void);
 
