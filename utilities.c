037d019de72dcf849c8f78eb2d0c20eaefc49dff
Jeffrey Lin <jeffrey@icurse.nl>
2017-11-25 21:34:59 -0500

    aash.c: add color definitions; colors to prompt

--- utilities.c
+++ utilities.c
@@ -1,6 +1,6 @@
 #include "aash.h"
 
 void errno_handler(int en) {
-  fprintf(stderr, "### Error %d: %s ###\n", en, strerror(en));
+  fprintf(stderr, "%sError %d: %s%s\n", CRED, en, strerror(en), CRESET);
 }
 
